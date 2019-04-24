9b78d204652138035abea19f63d8ed05a7e77226
Ivan Wiryadi <mikvadi.9@gmail.com>
2019-04-24 13:04:06 +0700

    phone

--- user/models.py
+++ user/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class User(models.Model):
     userID      = models.CharField(max_length=30, primary_key=True)
     userName    = models.CharField(max_length=50)
-    userEmail   = models.CharField()
-    cardNumber  = models.CharField()
+    userEmail   = models.CharField(max_length=50)
+    userPhone   = models.CharField(max_length=13)
+    cardNumber  = models.CharField(max_length=50)
     userMoney   = models.PositiveIntegerField()
