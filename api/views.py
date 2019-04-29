f87c3c5b00c69105377121e890361586a8088c1b
Rifda Annelies <rifdaaaz@gmail.com>
2019-04-29 12:47:58 +0700

    add disaster, update disaster, dan get capacity

--- api/views.py
+++ api/views.py
@@ -11,6 +11,9 @@ from parkingLot.models import Lot
 from user.models import User
 from payment.models import Payment
 from help.models import Help
+from disaster.models import Disaster
+from parkingLot.models import Lot
+
 
 def CheckInAPI(request, *args, **kwargs):
     #API to check in to park
@@ -22,6 +25,7 @@ def CheckInAPI(request, *args, **kwargs):
                 return HttpResponse("You are already in")
             else: 
                 parkloc = request.POST.get('location')
+<<<<<<< Updated upstream
                 lot = Lot.objects.get(lotName=parkloc)
                 t = Ticket(userID = User.objects.get(userID=user_id), location = lot)
                 
@@ -30,6 +34,14 @@ def CheckInAPI(request, *args, **kwargs):
                     lot.save()
                 t.save()
                 
+=======
+                u = User.objects.get(userID = user_id)
+                l = Lot.objects.get(lotName = parkloc)  
+                t = Ticket(userID = u, location = l)
+                t.save()
+                l.capacity=l.capacity-1
+                l.save()
+>>>>>>> Stashed changes
                 output = {
                     'ticketID' : str(t.ticketID),
                     'entryTime' : str(t.entryTime),
@@ -52,22 +64,49 @@ def CheckOutAPI(request, *args, **kwargs):
     #Needed parameters: userID
     if (request.method == 'POST'):
         user_id = request.POST.get('userID')
-        if (Ticket.objects.filter(userID=user_id, exitTime__isnull=True)):
-            t = Ticket.objects.get(userID=user_id, exitTime__isnull=True)
+        u = User.objects.get(userID = user_id)
+        if (Ticket.objects.filter(userID=u, exitTime__isnull=True)):
+            t = Ticket.objects.get(userID=u, exitTime__isnull=True)
             t.exitTime = datetime.datetime.now()
+<<<<<<< Updated upstream
             t.save()
             print(t)
             resp = __PaymentAPI(t)
+=======
+            t.save()    
+            l = t.location
+            l.capacity=l.capacity+1            
+            l.save()            
+            resp = PaymentAPI(t)
+>>>>>>> Stashed changes
             return resp
         else: 
             return HttpResponse("You have not checked in yet")
     else:
         return HttpResponseForbidden()
 
+<<<<<<< Updated upstream
 def __PaymentAPI(ticket, *args, **kwargs):
     u   = User.objects.get(userID = ticket.userID.userID)
     if (not u):
         return HttpResponse("error")
+=======
+def PaymentAPI(t, *args, **kwargs):
+    u = t.userID
+    price = 2000
+    dur = (t.exitTime-t.entryTime).seconds//3600 
+    remain = (t.exitTime-t.entryTime).seconds%3600 
+    print(dur)
+    total = dur*price 
+    if (remain):
+        total += price
+    if (u.userBalance >= total):
+        u.userBalance = u.userBalance - total
+        u.save()
+        p = Payment(userID = u, ticketID=t, duration=dur, amount=total)
+        p.save()
+        return HttpResponse("Payment successfull! \nIDR " + str(total) + " is deduced from your account\nYou have IDR" + str(u.userBalance) + " left")
+>>>>>>> Stashed changes
     else:
         price   = 2000
         dur     = (ticket.exitTime-ticket.entryTime).seconds//3600 
@@ -150,5 +189,73 @@ def CheckInLotAPI(request, *args, **kwargs):
             return HttpResponseBadRequest()
 
     else:
+<<<<<<< Updated upstream
         return HttpResponseForbidden()
         
+=======
+        return HttpResponse("Hello")
+
+def AddDisaster(request, *args, **kwargs):
+    if (request.method == 'POST'):
+        location = request.POST.get('location')
+        status = request.POST.get('status')
+        description = request.POST.get('description')
+        d = Disaster(location=location, status=status, description=description)
+        d.save()
+        output = {
+            'disasterID' : str(d.disasterID),
+            'status' : str(d.user.userID),
+            'location' : str(d.location),
+            'description' : str(d.description),
+        }
+        return HttpResponse(json.dumps(output))
+    else:
+        return HttpResponse("Hello")
+
+def UpdateDisaster(request, *args, **kwargs):
+    if (request.method == 'POST'):
+        disaster_id = request.POST.get('disasterID')
+        status = request.POST.get('status')
+        description = request.POST.get('description')
+        d = Disaster.objects.get(disasterID = disaster_id)
+        if (d):
+            d.status = status
+            d.updateTime = datetime.datetime.now()
+            d.description = description
+            d.save()
+            output = {
+                'disasterID' : str(d.disasterID),
+                'status' : str(d.user.userID),
+                'location' : str(d.location),
+                'description' : str(d.description),
+            }
+            return HttpResponse(json.dumps(output))
+        else: 
+            return HttpResponse("Disaster ID not valid")
+    else:
+        return HttpResponse("Hello")
+
+def getCapacity(request, *args, **kwargs):
+    if (request.method == 'GET'):
+        lot_name = request.GET.get('location')
+        l = Lot.objects.get(lotName = lot_name)
+        if (l):
+            output = {
+                'lotID' : str(l.lotID),
+                'location' : str(l.lotName),
+                'capacity' : str(l.capacity),
+            }
+            return HttpResponse(json.dumps(output))
+        else: 
+            return HttpResponse("Lot Name not valid")
+    else:
+        return HttpResponse("Hello")
+            
+
+#Yang perlu dikerjain
+#1. Location tuh perlu ada koordinat gitu biar bisa dikasih navigasi
+#4. Generate Laporan
+#5. Add booking
+#6. Manage booking
+#7. Navigate
+>>>>>>> Stashed changes
