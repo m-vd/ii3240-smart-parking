d0d41205a421121eb92a040fbc021a1571b4ec9f
Muhammad Taufik Rahman <18216030@std.stei.itb.ac.id>
2019-05-15 00:25:45 +0700

    apdet fe user lagi

--- userfe/views.py
+++ userfe/views.py
@@ -33,9 +33,15 @@ def countSlot(request, *args, **kwargs):
 	counter_sipil = 0
 	counter_sr_mobil = 0
 	counter_sr_motor = 0
+	counter_dalam_mobil = 0
+	counter_dalam_motor = 0
+
 	capacity_sipil = 0
 	capacity_sr_motor = 0
 	capacity_sr_mobil = 0
+	capacity_dalam_motor = 0
+	capacity_dalam_mobil = 0
+
 	tickets = Ticket.objects.all()
 	location = Lot.objects.all()
 
@@ -46,6 +52,10 @@ def countSlot(request, *args, **kwargs):
 			capacity_sr_motor = l.capacity
 		elif (l.lotID == 'Mobil_SR'):
 			capacity_sr_mobil = l.capacity
+		elif (l.lotID == 'Dalam_Motor_LabtekV' or l.lotID == 'Dalam_Motor_LabtekVIII'):
+			capacity_dalam_motor = capacity_dalam_motor + l.capacity
+		else:
+			capacity_dalam_mobil = capacity_dalam_mobil + l.capacity 
 
 	for t in tickets:
 		if (t.exitTime == None):
@@ -55,14 +65,23 @@ def countSlot(request, *args, **kwargs):
 				counter_sr_motor += 1
 			elif (t.location.lotID == 'Mobil_SR'):
 				counter_sr_mobil += 1
+			elif (t.location.lotID == 'Dalam_Motor_LabtekV' or t.location.lotID == 'Dalam_Motor_LabtekVIII'):
+				counter_dalam_motor += 1
+			else:
+				counter_dalam_mobil += 1
+
 	slot_sipil = capacity_sipil - counter_sipil
 	slot_sr_motor = capacity_sr_motor - counter_sr_motor
 	slot_sr_mobil = capacity_sr_mobil - counter_sr_mobil
+	slot_dalam_motor = capacity_dalam_motor - counter_dalam_motor
+	slot_dalam_mobil = capacity_dalam_mobil - counter_dalam_mobil
 
 	slot = {
 		'sipil' : slot_sipil,
 		'motor_sr' : slot_sr_motor,
 		'mobil_sr' : slot_sr_mobil,
+		'dalam_motor' : slot_dalam_motor,
+		'dalam_mobil' : slot_dalam_mobil,
 	}
 	return render(request, 'userfe/index.html', {'slot': slot})
 
